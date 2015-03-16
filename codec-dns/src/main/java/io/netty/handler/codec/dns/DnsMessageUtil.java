f53ce9c8e42def9b319b1a09e3c55b5aab7ab8eb
Trustin Lee <t@motd.kr>
2015-03-16 15:46:14 +0900

    Revamp DNS codec
    Motivation:
    
    There are various known issues in netty-codec-dns:
    
    - Message types are not interfaces, which can make it difficult for a
      user to implement his/her own message implementation.
    - Some class names and field names do not match with the terms in the
      RFC.
    - The support for decoding a DNS record was limited. A user had to
      encode and decode by him/herself.
    - The separation of DnsHeader from DnsMessage was unnecessary, although
      it is fine conceptually.
    - Buffer leak caused by DnsMessage was difficult to analyze, because the
      leak detector tracks down the underlying ByteBuf rather than the
      DnsMessage itself.
    - DnsMessage assumes DNS-over-UDP.
    
    Modifications:
    
    - Make all message types interfaces and add default implementations
    - Rename some classes, properties, and constants to match the RFCs
      - DnsResource -> DnsRecord
      - DnsType -> DnsRecordType
      - DnsClass -> DnsRecordClass
      - and many more
    - Add DnsRecordEncoder/DnsRecordDecoder and their default
      implementations
      - DnsRecord does not require RDATA to be ByteBuf anymore.
      - Add DnsRawRecord as the catch-all record type
    - Merge DnsHeader into DnsMessage
    - Make ResourceLeakDetector track AbstractDnsMessage
    - Remove DnsMessage.sender/recipient properties
      - Wrap DnsMessage with AddressedEnvelope
      - Add DatagramDnsQuest and DatagramDnsResponse for ease of use
      - Rename DnsQueryEncoder to DatagramDnsQueryEncoder
      - Rename DnsResponseDecoder to DatagramDnsResponseDecoder
    - Miscellaneous changes
      - Make StringUtil.EMPTY_STRING public
      - Add StringUtil.TAB
    
    Result:
    
    - Cleaner APi
    - Can support DNS-over-TCP more easily in the future
    - Reduced memory footprint in the default DnsQuery/Response
      implementations
    - Better leak tracking for DnsMessages
    - Possibility to introduce new DnsRecord types in the future and provide
      full record encoder/decoder implementation.

---
+++ codec-dns/src/main/java/io/netty/handler/codec/dns/DnsMessageUtil.java
@@ -0,0 +1,184 @@
+/*
+ * Copyright 2015 The Netty Project
+ *
+ * The Netty Project licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package io.netty.handler.codec.dns;
+
+import io.netty.channel.AddressedEnvelope;
+import io.netty.util.internal.StringUtil;
+
+import java.net.SocketAddress;
+import java.util.List;
+
+/**
+ * Provides some utility methods for DNS message implementations.
+ */
+final class DnsMessageUtil {
+
+    static StringBuilder appendQuery(StringBuilder buf, DnsQuery query) {
+        appendQueryHeader(buf, query);
+        appendAllRecords(buf, query);
+        return buf;
+    }
+
+    static StringBuilder appendResponse(StringBuilder buf, DnsResponse response) {
+        appendResponseHeader(buf, response);
+        appendAllRecords(buf, response);
+        return buf;
+    }
+
+    private static void appendQueryHeader(StringBuilder buf, DnsQuery msg) {
+        buf.append(StringUtil.simpleClassName(msg))
+           .append('(');
+
+        appendAddresses(buf, msg)
+           .append(msg.id())
+           .append(", ")
+           .append(msg.opCode());
+
+        if (msg.isRecursionDesired()) {
+            buf.append(", RD");
+        }
+        if (msg.z() != 0) {
+            buf.append(", Z: ")
+               .append(msg.z());
+        }
+        buf.append(')');
+    }
+
+    private static void appendResponseHeader(StringBuilder buf, DnsResponse msg) {
+        buf.append(StringUtil.simpleClassName(msg))
+           .append('(');
+
+        appendAddresses(buf, msg)
+           .append(msg.id())
+           .append(", ")
+           .append(msg.opCode())
+           .append(", ")
+           .append(msg.code())
+           .append(',');
+
+        boolean hasComma = true;
+        if (msg.isRecursionDesired()) {
+            hasComma = false;
+            buf.append(" RD");
+        }
+        if (msg.isAuthoritativeAnswer()) {
+            hasComma = false;
+            buf.append(" AA");
+        }
+        if (msg.isTruncated()) {
+            hasComma = false;
+            buf.append(" TC");
+        }
+        if (msg.isRecursionAvailable()) {
+            hasComma = false;
+            buf.append(" RA");
+        }
+        if (msg.z() != 0) {
+            if (!hasComma) {
+                buf.append(',');
+            }
+            buf.append(" Z: ")
+               .append(msg.z());
+        }
+
+        if (hasComma) {
+            buf.setCharAt(buf.length() - 1, ')');
+        } else {
+            buf.append(')');
+        }
+    }
+
+    private static StringBuilder appendAddresses(StringBuilder buf, DnsMessage msg) {
+
+        if (!(msg instanceof AddressedEnvelope)) {
+            return buf;
+        }
+
+        @SuppressWarnings("unchecked")
+        AddressedEnvelope<?, SocketAddress> envelope = (AddressedEnvelope<?, SocketAddress>) msg;
+
+        SocketAddress addr = envelope.sender();
+        if (addr != null) {
+            buf.append("from: ")
+               .append(addr)
+               .append(", ");
+        }
+
+        addr = envelope.recipient();
+        if (addr != null) {
+            buf.append("to: ")
+               .append(addr)
+               .append(", ");
+        }
+
+        return buf;
+    }
+
+    private static void appendAllRecords(StringBuilder buf, DnsMessage msg) {
+        appendQuestions(buf, msg);
+        if (msg.answerCount() != 0) {
+            appendRecords(buf, msg.answers());
+        }
+        if (msg.authorityRecordCount() != 0) {
+            appendRecords(buf, msg.authorityRecords());
+        }
+        appendAdditionalRecords(buf, msg);
+    }
+
+    private static void appendQuestions(StringBuilder buf, DnsMessage msg) {
+        final int questionCount = msg.questionCount();
+        if (questionCount == 0) {
+            return;
+        }
+
+        if (questionCount == 1) {
+            buf.append(StringUtil.NEWLINE)
+               .append(StringUtil.TAB)
+               .append(msg.question());
+        } else {
+            for (DnsQuestion q: msg.questions()) {
+                buf.append(StringUtil.NEWLINE)
+                   .append(StringUtil.TAB)
+                   .append(q);
+            }
+        }
+    }
+
+    private static void appendAdditionalRecords(StringBuilder buf, DnsMessage msg) {
+        final int additionalRecordCount = msg.additionalRecordCount();
+        if (additionalRecordCount == 0) {
+            return;
+        }
+
+        if (additionalRecordCount == 1) {
+            buf.append(StringUtil.NEWLINE)
+               .append(StringUtil.TAB)
+               .append(msg.additionalRecord());
+        } else {
+            appendRecords(buf, msg.additionalRecords());
+        }
+    }
+
+    private static void appendRecords(StringBuilder buf, List<DnsRecord> records) {
+        for (DnsRecord r: records) {
+            buf.append(StringUtil.NEWLINE)
+               .append(StringUtil.TAB)
+               .append(r);
+        }
+    }
+
+    private DnsMessageUtil() { }
+}
